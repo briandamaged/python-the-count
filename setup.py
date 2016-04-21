a82a3f8a8ecf0cd7cab44b7e32ba4d20d9ae45e8
Brian Lauber <constructible.truth@gmail.com>
2016-04-21 11:30:27 -0400

    Bumped version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'the_count',
-    version = '0.1.0',
+    version = '0.2.0',
     description = 'Handy class for counting things',
     author = 'Brian Lauber',
     author_email = 'constructible.truth@gmail.com',
